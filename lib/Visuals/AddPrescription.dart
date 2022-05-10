95a02f32bde86c3dcf58f3bb44820f1756799a2d
UnculturedCat <76996825+UnculturedCat@users.noreply.github.com>
2022-05-10 13:30:01 +0200

    Done again added icons and dismission of virtual keyboard on addprescription page

--- lib/Visuals/AddPrescription.dart
+++ lib/Visuals/AddPrescription.dart
@@ -13,171 +13,179 @@ class _AddPrescriptionPageState extends State<AddPrescriptionPage>
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(
-          "Add Prescription",
-          style: TextStyle(
-              color: Color.fromRGBO(
-                26,
-                176,
-                96,
-                1.0,
-              ),
-              fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Color.fromRGBO(
-          255,
-          255,
-          255,
-          0.0,
-        ),
-        shadowColor: Color.fromARGB(
-          26,
-          255,
-          255,
-          255,
-        ),
-        actionsIconTheme: IconThemeData(
-          color: Colors.blue,
-        ),
-        actions: [
-          // Container(
-          //   // padding: EdgeInsets.only(left: 100),
-          //   child: IconButton(
-          //     onPressed: () {
-          //       setDay();
-          //     },
-          //     icon: Icon(CupertinoIcons.calendar),
-          //   ),
-          // )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.only(
-          left: 20,
-          right: 20,
-        ),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: Image.asset("Medicine.jpeg").image,
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-              Colors.black54,
-              BlendMode.darken,
-            ),
+    return GestureDetector(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(
+            "Add Prescription",
+            style: TextStyle(
+                color: Color.fromRGBO(
+                  26,
+                  176,
+                  96,
+                  1.0,
+                ),
+                fontWeight: FontWeight.bold),
           ),
-          //backgroundBlendMode: BlendMode.
+          backgroundColor: Color.fromRGBO(
+            255,
+            255,
+            255,
+            0.0,
+          ),
+          shadowColor: Color.fromARGB(
+            26,
+            255,
+            255,
+            255,
+          ),
+          actionsIconTheme: IconThemeData(
+            color: Colors.blue,
+          ),
+          actions: [
+            // Container(
+            //   // padding: EdgeInsets.only(left: 100),
+            //   child: IconButton(
+            //     onPressed: () {
+            //       setDay();
+            //     },
+            //     icon: Icon(CupertinoIcons.calendar),
+            //   ),
+            // )
+          ],
         ),
-        child: ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Prescription Name",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          CupertinoIcons.tag,
-                          size: 15,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        filled: true,
-                        fillColor: Color.fromRGBO(240, 240, 240, 1.0),
-                      ),
-                    ),
-                  ),
-                ],
+        body: Container(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+          ),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Image.asset("Medicine.jpeg").image,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.black54,
+                BlendMode.darken,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(top: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Prescription Description",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
+            //backgroundBlendMode: BlendMode.
+          ),
+          child: ListView(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Prescription Name",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    child: TextFormField(
-                      minLines: 5,
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(),
-                        prefixIcon: Icon(
-                          CupertinoIcons.tag,
-                          size: 15,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        filled: true,
-                        fillColor: Color.fromRGBO(240, 240, 240, 1.0),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 40),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            elevation: 10,
-                            primary: Color.fromRGBO(26, 176, 96, 1.0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
+                    Container(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(),
+                          prefixIcon: Icon(
+                            CupertinoIcons.tag,
+                            size: 15,
+                            color: Colors.black,
                           ),
-                          onPressed: () {},
-                          child: Container(
-                            padding: EdgeInsets.all(
-                              10,
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          filled: true,
+                          fillColor: Color.fromRGBO(240, 240, 240, 1.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Prescription Description",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: TextFormField(
+                        minLines: 5,
+                        maxLines: null,
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(),
+                          prefixIcon: Icon(
+                            CupertinoIcons.tag,
+                            size: 15,
+                            color: Colors.black,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          filled: true,
+                          fillColor: Color.fromRGBO(240, 240, 240, 1.0),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 10,
+                              primary: Color.fromRGBO(26, 176, 96, 1.0),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
                             ),
-                            child: Text(
-                              "Add",
-                              style: TextStyle(
-                                color: Colors.white,
+                            onPressed: () {},
+                            child: Container(
+                              padding: EdgeInsets.all(
+                                10,
+                              ),
+                              child: Text(
+                                "Add",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
     );
   }
 
